9c85e1014e93fe16ca297ae17ab18e77d6a53886
Bradford1040 <bradfordaldenadams@gmail.com>
2023-11-14 17:51:02 -0500

    Updated Ender 5 + Build

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -24,30 +24,32 @@
 ////////////////////////////
 // VENDOR VERSION EXAMPLE //
 ////////////////////////////
-
 /**
+
  * Marlin release version identifier
  */
-//#define SHORT_BUILD_VERSION "bugfix-2.1.x"
+#define SHORT_BUILD_VERSION "E5+_V11.14.23"
 
 /**
  * Verbose version identifier which should contain a reference to the location
  * from where the binary was downloaded or the source code was compiled.
  */
-//#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION
+#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION " SKR_3(STM32H723VG_btt), bugfix-2.1.x"
 
 /**
  * The STRING_DISTRIBUTION_DATE represents when the binary file was built,
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-//#define STRING_DISTRIBUTION_DATE "2023-11-14"
+//#define STRING_DISTRIBUTION_DATE "2023-10-29"
+#define STRING_DISTRIBUTION_DATE __DATE__
+#define STRING_DISTRIBUTION_TIME __TIME__
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
  */
-//#define MACHINE_NAME "3D Printer"
-
+#define MACHINE_NAME "Ender5+"
+//#define CUSTOM_MACHINE_NAME "Ender5+_Punisher" //Listed in Configuration.h
 /**
  * The SOURCE_CODE_URL is the location where users will find the Marlin Source
  * Code which is installed on the device. In most cases —unless the manufacturer
@@ -55,17 +57,23 @@
  * Marlin repository.
  */
 //#define SOURCE_CODE_URL "github.com/MarlinFirmware/Marlin"
+#define SOURCE_CODE_URL "https://github.com/Bradford1040/Marlin_Bugfix-2.1.x/tree/Ender_Five_Plus"
 
 /**
  * Default generic printer UUID.
  */
 //#define DEFAULT_MACHINE_UUID "cede2a2f-41a2-4748-9b12-c55c62f367ff"
+/**
+ * Hammers generic printer UUID
+ * For Ender 5 Plus
+ */
+//#define   DEFAULT_MACHINE_UUID "6db1c193-dd5a-4e84-9d89-88cea6471af8"
 
 /**
  * The WEBSITE_URL is the location where users can get more information such as
  * documentation about a specific Marlin release.
  */
-//#define WEBSITE_URL "marlinfw.org"
+#define WEBSITE_URL "https://marlinfw.org"
 
 /**
  * Set the vendor info the serial USB interface, if changable
