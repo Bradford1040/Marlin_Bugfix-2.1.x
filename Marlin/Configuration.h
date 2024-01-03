3717fc915273f5957d29fbad47856117153578f7
Bradford1040 <bradfordaldenadams@gmail.com>
2024-01-03 03:28:52 -0500

    refresh 1

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -61,8 +61,8 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(none, default config)" // Who made the changes.
-//#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
+#define STRING_CONFIG_H_AUTHOR "(Hammer)" // Who made the changes.
+#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 // @section machine
 
